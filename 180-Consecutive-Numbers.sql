70d12fa53829fee6bc79bf0030e3a968d0f130a2
Shui Yu <55073783+Shui-Yu@users.noreply.github.com>
2020-03-30 17:00:48 -0400

    Update 180-Consecutive-Numbers.sql

--- 180-Consecutive-Numbers.sql
+++ 180-Consecutive-Numbers.sql
@@ -8,6 +8,7 @@ FROM (
     ) tb
 WHERE previous = num and num = next;
 
+
 -- Join
 SELECT DISTINCT l1.num consecutivenums
 FROM logs l1
@@ -15,4 +16,3 @@ JOIN logs l2
 ON l1.id = l2.id-1 AND l1.num = l2.num
 JOIN logs l3
 ON l2.id = l3.id-1 AND l2.num = l3.num;
-
