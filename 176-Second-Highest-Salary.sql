8ff6b0d2e30e025c749bc68de2b0af601ee7514b
Shui Yu <55073783+Shui-Yu@users.noreply.github.com>
2020-03-30 11:48:35 -0400

    Add files via upload

---
+++ 176-Second-Highest-Salary.sql
@@ -0,0 +1,32 @@
+-- Subquery
+SELECT MAX(salary) as secondhighestsalary
+FROM employee
+WHERE salary < (SELECT MAX(salary) FROM employee);
+
+
+-- Window function
+-- DENSE_RANK()
+SELECT AVG(salary) secondhighestsalary
+FROM (
+    SELECT salary,
+      DENSE_RANK() OVER (ORDER BY salary DESC) rank
+    FROM employee
+    ) tb
+WHERE rank = 2;
+
+-- ROW_NUMBER()
+SELECT salary secondhighestsalary
+FROM (
+    SELECT salary,
+      ROW_NUMBER() OVER (ORDER BY salary DESC) rank
+    FROM employee
+    ) tb
+WHERE rank = 2;
+
+
+-- Offset fetch
+SELECT salary AS secondhighestsalary
+FROM employee
+ORDER BY salary DESC
+OFFSET 1 ROW
+FETCH NEXT 1 ROW ONLY;
