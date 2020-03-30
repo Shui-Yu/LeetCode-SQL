537b4fb2ad4f25c8ba154e687e659f875a930239
Shui Yu <55073783+Shui-Yu@users.noreply.github.com>
2020-03-30 13:57:15 -0400

    Add files via upload

---
+++ 177-Nth-Highest-Salary.sql
@@ -0,0 +1,25 @@
+-- Window function
+CREATE FUNCTION getNthHighestSalary(@N INT) RETURNS INT AS
+BEGIN
+    RETURN (
+        SELECT AVG(salary)
+        FROM (
+            SELECT salary,
+              DENSE_RANK() OVER (ORDER BY salary DESC) rank
+            FROM employee
+            ) tb
+        WHERE rank = @N
+    );
+END
+
+-- Offset fetch
+CREATE FUNCTION getNthHighestSalary(@N INT) RETURNS INT AS
+BEGIN
+    RETURN (
+        SELECT DISTINCT salary
+        FROM employee
+        ORDER BY salary DESC
+        OFFSET @N-1 ROWS
+        FETCH FIRST 1 ROW ONLY
+    );
+END
