799fc52ab4689ed182c21fe16603fbb19e9ec8b6
Shui Yu <55073783+Shui-Yu@users.noreply.github.com>
2021-02-22 22:02:02 -0500

    Update 177-Nth-Highest-Salary.sql

--- 177-Nth-Highest-Salary.sql
+++ 177-Nth-Highest-Salary.sql
@@ -1,4 +1,4 @@
--- SQL Server
+-- SQL Server: OFFSET FETCH
 CREATE FUNCTION getNthHighestSalary(@N INT) RETURNS INT AS
 BEGIN
     RETURN (
@@ -8,4 +8,18 @@ BEGIN
         OFFSET @N-1 ROWS
         FETCH FIRST 1 ROW ONLY
     );
+END
+
+-- MySQL: Window Function 
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+      WITH tab AS(
+      SELECT Salary, DENSE_RANK() OVER(ORDER BY Salary DESC) AS 'rank'
+      FROM Employee)
+      
+      SELECT DISTINCT Salary
+      FROM tab
+      WHERE tab.rank = N   
+  );
 END
