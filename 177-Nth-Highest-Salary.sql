acaf579fa503047a5661404e3af5357114cc001a
Shui Yu <55073783+Shui-Yu@users.noreply.github.com>
2021-02-22 21:54:03 -0500

    Update 177-Nth-Highest-Salary.sql

--- 177-Nth-Highest-Salary.sql
+++ 177-Nth-Highest-Salary.sql
@@ -1,24 +1,10 @@
--- Window function
+-- SQL Server
 CREATE FUNCTION getNthHighestSalary(@N INT) RETURNS INT AS
 BEGIN
     RETURN (
-        SELECT AVG(salary)
-        FROM (
-            SELECT salary,
-              DENSE_RANK() OVER (ORDER BY salary DESC) rank
-            FROM employee
-            ) tb
-        WHERE rank = @N
-    );
-END
-
--- OFFSET FETCH
-CREATE FUNCTION getNthHighestSalary(@N INT) RETURNS INT AS
-BEGIN
-    RETURN (
-        SELECT DISTINCT salary
-        FROM employee
-        ORDER BY salary DESC
+        SELECT DISTINCT Salary
+        FROM Employee
+        ORDER BY Salary DESC
         OFFSET @N-1 ROWS
         FETCH FIRST 1 ROW ONLY
     );
